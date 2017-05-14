311ced1a1fdfe25e65c093b066ef811b0a5dfd45
Antonio Verardi <antonio@yelp.com>
2017-05-14 17:53:40 +0100

    Second run

--- decorators/collect.py
+++ decorators/collect.py
@@ -1,9 +1,15 @@
 from importlib import import_module
 import glob
 import os
+import inspect
+from statmonster import Log
 
 if __name__ == "__main__":
     for file_path in glob.iglob(os.path.join("users", "*.py")):
         module_name = '.'.join(file_path[:-3].split('/'))
-        module = import_module(module_name)
-        print(module)
+        import_module(module_name)
+
+    module = import_module("users.logs")
+    for name, obj in inspect.getmembers(module):
+        if isinstance(obj, Log):
+            print(name)
