01e14e20a03d375b09f841a95976f7f9aed55f1f
Antonio Verardi <antonio@yelp.com>
2017-05-14 14:25:53 +0100

    More to log

--- log.py
+++ log.py
@@ -1,4 +1,5 @@
 import json
+from emails import send_email
 
 
 def decode_json(line):
@@ -9,6 +10,19 @@ class Log:
     def __init__(self, name, codec=decode_json):
         self.name = name
         self.codec = codec
+        self.fns = []
 
     def decode(self, line):
         return self.codec(line)
+
+    def process(self, line):
+        entry = self.decode(line)
+        for fn in self.fns:
+            try:
+                yield from fn(entry, self.name)
+            except Exception as e:
+                send_email(fn.owners, e)
+
+    def register(self, fn):
+        self.fns.append(fn)
+        return fn
