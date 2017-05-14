f3f4968b8b5691c7853c140fe1c65b1a3d2d3ae9
Antonio Verardi <antonio@yelp.com>
2017-05-14 17:41:36 +0100

    Fixes and stubs

--- decorators/collect.py
+++ decorators/collect.py
@@ -3,9 +3,7 @@ import glob
 import os
 
 if __name__ == "__main__":
-    for file_path in glob.iglob(os.pathnamepath.pathnamejoin("users", "*.py")):
-        name = os.path.basename(file_path)[:-3]
-        if not name.startswith('_'):
-            module_name = '.'.join(name[:-3].split('/'))
-            module = import_module(module_name)
-            print(module)
+    for file_path in glob.iglob(os.path.join("users", "*.py")):
+        module_name = '.'.join(file_path[:-3].split('/'))
+        module = import_module(module_name)
+        print(module)
