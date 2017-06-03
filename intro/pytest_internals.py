1874987ba2808d470e13598edeee9c5c3272ebef
Antonio Verardi <antonio@yelp.com>
2017-06-03 17:08:44 +0100

    More pytest internals

--- intro/pytest_internals.py
+++ intro/pytest_internals.py
@@ -6,3 +6,19 @@ def fixture(scope="function", params=None, autouse=False, ids=None, name=None):
 	if params is not None and not isinstance(params, (list, tuple)):
 			params = list(params)
 	return FixtureFunctionMarker(scope, params, autouse, ids=ids, name=name)
+
+
+class FixtureFunctionMarker:
+    def __init__(self, scope, params, autouse=False, ids=None, name=None):
+        self.scope = scope
+        self.params = params
+        self.autouse = autouse
+        self.ids = ids
+        self.name = name
+
+    def __call__(self, function):
+        if isclass(function):
+            raise ValueError(
+                    "class fixtures not supported (may be in the future)")
+        function._pytestfixturefunction = self
+		return function
