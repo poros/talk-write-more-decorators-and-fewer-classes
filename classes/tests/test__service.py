722f9972b26411b161a1a0027d64b9e61753a41f
Antonio Verardi <antonio@yelp.com>
2017-06-17 04:46:40 +0100

    Remove useless test class

--- classes/tests/test__service.py
+++ classes/tests/test__service.py
@@ -3,21 +3,21 @@ from metrics._service import ServiceCountBaseTrigger
 from statmonster import Counter
 
 
-class TestServiceCountBaseTrigger():
-    @pytest.fixture
-    def trigger(self):
-        return type(
-            "TestTrigger",
-            (ServiceCountBaseTrigger,),
-            {"metric_name": "test", "owners": ["foo@yelp.com"]},
-        )()
+@pytest.fixture
+def trigger(self):
+    return type(
+        "TestTrigger",
+        (ServiceCountBaseTrigger,),
+        {"metric_name": "test", "owners": ["foo@yelp.com"]},
+    )()
 
-    def test_digest(self, trigger):
-        entry = {
-            "time": 123456,
-            "status": 200,
-            "method_name": "GET",
-            "blob": "bla",
-        }
-        expected = Counter("test.request_count", 123456, 1, {"method": "GET"})
-        assert expected in list(trigger.digest(entry))
+
+def test_digest(self, trigger):
+    entry = {
+        "time": 123456,
+        "status": 200,
+        "method_name": "GET",
+        "blob": "bla",
+    }
+    expected = Counter("test.request_count", 123456, 1, {"method": "GET"})
+    assert expected in list(trigger.digest(entry))
